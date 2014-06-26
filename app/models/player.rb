893486eaee35be60a17ad2877a48c10c7be7cfe8
Colin Campbell <colindcamp@gmail.com>
2014-06-25 20:10:15 -0700

    pushing to heroku

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,10 @@ class Player < ActiveRecord::Base
   has_many :football_stats
   has_many :stats
   has_many :games, through: :stats
+
+  before_destroy :destroy_children
+
+  def destroy_children
+  	Stat.where(player_id: self.id).destroy_all
+  end
 end
